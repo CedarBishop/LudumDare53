9b2526d1673be77167c55f93b3fbd05c2007b8e3
Ivan Karyakin <48420289+1whywhy1@users.noreply.github.com>
2023-04-29 22:15:54 +1000

    Added TMPro, Added a first draft of tileset in 64, added Text stating goal Cruise Control

--- Assets/Scripts/SpeedometerController.cs
+++ Assets/Scripts/SpeedometerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -8,20 +9,31 @@ using UnityEngine.UI;
 public class SpeedometerController : MonoBehaviour
 {
     [SerializeField] Player player;
+
+    // Speedometre
     [SerializeField] Image forwardSpeedometre;
     [SerializeField] Image reverseSpeedometre;
-
-   // private float currentSpeed;
     private float maxForwardSpeed;
     private float maxReverseSpeed;
-    private bool isReversing;
+
+    // Cruise Control
+    [SerializeField] RectTransform cruiseControlArrow;
+    private float forwardImageSize;
+    private float reverseImageSize;
+    [SerializeField] private TMP_Text cruiseSpeedTMP;
+
+    //Debug
+    public float var1;
+    public float var2;
 
     void Start()
     {
+        forwardImageSize = forwardSpeedometre.transform.localScale.x;
+        reverseImageSize = reverseSpeedometre.transform.localScale.x;
+
         player = FindObjectOfType<Player>();
         maxForwardSpeed = player.GetMaxForwardSpeed();
         maxReverseSpeed = player.GetMaxReverseSpeed();
-        isReversing = player.GetIsReversing();
     }
 
 
@@ -33,17 +45,34 @@ public class SpeedometerController : MonoBehaviour
         if(currentSpeed > 0)
         {
             forwardSpeedometre.fillAmount = player.GetCurrentSpeed() / maxForwardSpeed;
+           SetCurrentCruiseSpeedUI(forwardImageSize, maxForwardSpeed);
         }
         else if (currentSpeed < 0)
         {
             reverseSpeedometre.fillAmount = player.GetCurrentSpeed() / maxReverseSpeed;
+           SetCurrentCruiseSpeedUI(reverseImageSize, maxReverseSpeed);
         }
         else
         {
             forwardSpeedometre.fillAmount = 0;
             reverseSpeedometre.fillAmount = 0;
+            cruiseControlArrow.position = new Vector3 (0f, cruiseControlArrow.position.y, cruiseControlArrow.position.z);
         }
+        cruiseSpeedTMP.SetText(("Cruise: " + player.GetCruiseControlSpeed()));
 
         //Debug.Log(player.GetCurrentSpeed() / maxForwardSpeed);
+    }
+
+
+    //private void OnValidate()
+    //{
+    //    SetCurrentCruiseSpeedUI(var1,var2);
+    //}
+
+    private void SetCurrentCruiseSpeedUI( float imageSize, float maxSpeed)
+    {
+        cruiseControlArrow.localPosition = new Vector3((player.GetCruiseControlSpeed() / maxSpeed) * imageSize, cruiseControlArrow.position.y, cruiseControlArrow.position.z);
+        //cruiseControlArrow.position = new Vector3(imageSize, maxSpeed, 0);
+        player.GetCruiseControlSpeed();
     }
 }
