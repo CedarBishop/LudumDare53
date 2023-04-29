629fada24f63872d7a290e13061fd0c02d0a747e
cbishop <47874454+CedarBishop@users.noreply.github.com>
2023-04-29 19:11:40 +1000

    Created basic pickup drop off mechanic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
 
-    public PickupDropoffLocations[] pickupDropoffLocations;
+    public LevelManager levelManager;
 
     public Trip currentTrip = null;
+
+    public Action<Trip> AcceptedFareEvent;
+    public Action<Trip> StartedFareEvent;
+    public Action<Trip> FinishedFareEvent;
 
     private void Awake()
     {
@@ -22,9 +27,44 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        SelectTrip(new Trip(levelManager.pickupDropoffLocations));
+    }
+
+    private void Update()
+    {
+        if (currentTrip != null)
+        {
+            if (currentTrip.startedFare)
+            {
+                currentTrip.time += Time.deltaTime;
+            }
+        }
+    }
+
     public void SelectTrip (Trip trip)
     {
         currentTrip = trip;
+        currentTrip.pickupLocation.gameObject.SetActive(true);
+        AcceptedFareEvent(currentTrip);
+    }
+
+    public void PickedUpPassenger()
+    {
+        currentTrip.pickupLocation.gameObject.SetActive(false);
+        currentTrip.dropoffLocation.gameObject.SetActive(true);
+        currentTrip.time = 0;
+        currentTrip.startedFare = true;
+        StartedFareEvent(currentTrip);
+    }
+
+    public void DroppedOffPasenger()
+    {
+        currentTrip.dropoffLocation.gameObject.SetActive(false);
+        currentTrip.startedFare = false;
+        print("Finished trip");
+        FinishedFareEvent(currentTrip);
     }
 }
 
@@ -32,20 +72,21 @@ public class Trip
 {
     public Trip (PickupDropoffLocations[] pickupDropoffLocations)
     {
-        PickupDropoffLocations potentialPickupLocation = pickupDropoffLocations[Random.Range(0, pickupDropoffLocations.Length)];
+        PickupDropoffLocations potentialPickupLocation = pickupDropoffLocations[UnityEngine.Random.Range(0, pickupDropoffLocations.Length)];
         PickupDropoffLocations potentialDropoffLocation = null;
         do
         {
-            potentialDropoffLocation = pickupDropoffLocations[Random.Range(0, pickupDropoffLocations.Length)];
+            potentialDropoffLocation = pickupDropoffLocations[UnityEngine.Random.Range(0, pickupDropoffLocations.Length)];
         } while (potentialDropoffLocation == potentialPickupLocation);
         pickupLocation = potentialPickupLocation;
         dropoffLocation = potentialDropoffLocation;
 
         // calculate starting fare
     }
-    PickupDropoffLocations pickupLocation;
-    PickupDropoffLocations dropoffLocation;
+    public PickupDropoffLocations pickupLocation;
+    public PickupDropoffLocations dropoffLocation;
     public float rating;
     public float fare;
     public float time;
+    public bool startedFare = false;
 }
--- Assets/Scripts/PickupDropoffLocations.cs
+++ Assets/Scripts/PickupDropoffLocations.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PickupDropoffLocations : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float timeToComplete;
+    private float timer;
     void Start()
     {
-        
+        bool shouldSetActive = GameManager.instance.currentTrip != null && GameManager.instance.currentTrip.pickupLocation == this;
+        gameObject.SetActive(shouldSetActive);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        timer = 0;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (GameManager.instance.currentTrip == null)
+        {
+            return;
+        }
+        timer += Time.fixedDeltaTime;
+        if (timer >= timeToComplete)
+        {
+            if (GameManager.instance.currentTrip.pickupLocation.gameObject == gameObject)
+            {
+                GameManager.instance.PickedUpPassenger();
+            }
+            if (GameManager.instance.currentTrip.dropoffLocation.gameObject == gameObject)
+            {
+                GameManager.instance.DroppedOffPasenger();
+            }
+        }
     }
 }
