d16bd7ba56f134f4c530cefb772f276df1ab062d
cbishop <47874454+CedarBishop@users.noreply.github.com>
2023-04-30 10:20:45 +1000

    Created fuel station and fuel usage logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -26,10 +26,12 @@ public class Player : MonoBehaviour
     private float currentSpeedInputValue;
 
     private Rigidbody2D rigidbody;
+    private FuelTank fuelTank;
 
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();    
+        rigidbody = GetComponent<Rigidbody2D>();
+        fuelTank = GetComponent<FuelTank>();
     }
 
     void FixedUpdate()
@@ -51,13 +53,33 @@ public class Player : MonoBehaviour
         currentSpeed -= brakeSpeed * Time.fixedDeltaTime * (scaledTargetSpeed - currentSpeed < 0 ? 1 : 0);
 
         isReversing = currentSpeed < 0;
+        if (currentSpeed > maxForwardSpeed)
+        {
+            currentSpeed = maxForwardSpeed;
+        }
+
+        if (currentSpeed < maxReverseSpeed)
+        {
+            currentSpeed = maxReverseSpeed;
+        }
+
         rigidbody.velocity = transform.up * currentSpeed;
         transform.Rotate(0, 0, currentHorizontalValue * turnSpeed * rigidbody.velocity.magnitude * Time.fixedDeltaTime * (isReversing ? 1 : -1));
-
+        UseFuel();
     }
 
     void NewDrivingControlsUpdate()
     {
+        if (fuelTank.GetFuelLevel() <= 0.0f)
+        {
+            if (currentSpeed >= 0.0f)
+            {
+                currentSpeed -= brakeSpeed * Time.fixedDeltaTime;
+                rigidbody.velocity = transform.up * currentSpeed;
+                transform.Rotate(0, 0, currentHorizontalValue * turnSpeed * rigidbody.velocity.magnitude * Time.fixedDeltaTime * (isReversing ? 1 : -1));
+            }
+            return;
+        }
         if (currentSpeedInputValue > 0.1f)
         {
             currentSpeed += accelerationSpeed * Time.fixedDeltaTime;
@@ -76,10 +98,31 @@ public class Player : MonoBehaviour
             currentSpeed -= reverseAccelerationSpeed * Time.fixedDeltaTime;
         }
 
+        if (currentSpeed > maxForwardSpeed * realWorldToInGameScalar)
+        {
+            currentSpeed = maxForwardSpeed * realWorldToInGameScalar;
+        }
+
+        if (currentSpeed < maxReverseSpeed * realWorldToInGameScalar)
+        {
+            currentSpeed = maxReverseSpeed * realWorldToInGameScalar;
+        }
+
         isReversing = currentSpeed < 0;
         rigidbody.velocity = transform.up * currentSpeed;
         transform.Rotate(0, 0, currentHorizontalValue * turnSpeed * rigidbody.velocity.magnitude * Time.fixedDeltaTime * (isReversing ? 1 : -1));
+        UseFuel();
+    }
 
+    void UseFuel()
+    {
+        float fuelUsageAmountThisFrame = fuelTank.fuelUsageScalar * Mathf.Abs(currentSpeed) *Time.fixedDeltaTime;
+        fuelTank.SetFuelLevel(fuelTank.GetFuelLevel() - fuelUsageAmountThisFrame);
+
+        if (fuelTank.GetFuelLevel() <= 0.0f)
+        {
+            // Do empty tank logic
+        }
     }
 
     public void OnIncreaseSpeed()
@@ -149,6 +192,13 @@ public class Player : MonoBehaviour
     public float GetMaxReverseSpeed()
     {
         return maxReverseSpeed;
+    }
+
+    public bool TryRefuel()
+    {
+        //TODO: calculate fuel price and difference and lower from total money
+        fuelTank.SetFuelLevel(fuelTank.maxFuelLevel);
+        return true;
     }
 
 
